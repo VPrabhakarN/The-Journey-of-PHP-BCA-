f65fa91e9251e92e71a66444639ae65368286e7e
vinnyyyt <vijaynagane19@gmail.com>
2023-10-28 15:49:16 +0530

    PHP  variables

---
+++ php_variables.php
@@ -0,0 +1,12 @@
+<?php 
+       //here to define a variable which store a string 
+       //each variable in php start with $
+       $txt = "Hello World";
+
+       //now we define another variable which store an integer 
+       $x = 5;
+       
+       //now we print both values by calling or printing variables 
+       echo $txt;
+       echo $x;
+?>
