e3d890e4691dcec73f7cd7ff8bdb8e90c69e1cb8
vinnyyyt <vijaynagane19@gmail.com>
2023-10-29 09:45:29 +0530

    php programs

---
+++ array_searsh().php
@@ -0,0 +1,9 @@
+<?php 
+     
+      //php program to perform 'search()' function
+      $season = array("Summer", "Mansoon","Winter");
+
+      //using 'search()' function 
+      $key - array_search("Winter",$season);
+      echo $key;
+?>
