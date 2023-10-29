d8588096089cb067d2666c3d6a5484853ed37095
vinnyyyt <vijaynagane19@gmail.com>
2023-10-29 06:25:02 +0530

    php programs

---
+++ if_else_statement.php
@@ -0,0 +1,28 @@
+<?php
+      
+      /* 
+         SYNTAX :
+                   if(condition)
+                   {
+                    // code to be executed if 'condition' is true
+                   } 
+                   else 
+                   {
+                    // code to be executed if 'condition'  is false 
+                   }
+      */
+
+      //php program to perform if else statement
+      $vita = 1923;
+      $tr = 2000;
+
+      //using if else startement
+      if($vita > $tr)
+      {
+        echo"VITA is great than TR";
+      }
+      else 
+      {
+        echo"TR is greater than VITA";
+      }
+?>
