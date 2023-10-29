665bc735250171b882070997af403d4d20afbfe0
vinnyyyt <vijaynagane19@gmail.com>
2023-10-29 10:45:00 +0530

    php programs

---
+++ function(WO_argu_W_rv).php
@@ -0,0 +1,13 @@
+<?php 
+       
+      //In this php program to define a function without argument with return value
+      function add()
+      {
+        $x = 10;
+        $y = 20;
+        $z = $x + $y;
+        return $z;
+      }
+      $p = add();
+      echo"Addition : $p";
+?>
