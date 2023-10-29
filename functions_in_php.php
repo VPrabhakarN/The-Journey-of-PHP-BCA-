d3a8684265ee75a844885ad2a115c391108016c3
vinnyyyt <vijaynagane19@gmail.com>
2023-10-29 10:21:04 +0530

    php programs

---
+++ functions_in_php.php
@@ -0,0 +1,17 @@
+<?php
+      
+      /* In php there are 2 type of functions
+         1) Built-in Functions
+         2) User Defined Functions */ 
+
+    // In this php function we define an user defined function and call the function
+
+    #Defining a function
+    function myfunction()
+    {
+        echo"This is an user defined function";
+    }
+
+    #calling function
+    myfunction();
+?>
