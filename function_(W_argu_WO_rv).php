7319ec785ccd156b0f37620fdfb9a7ad651e0840
vinnyyyt <vijaynagane19@gmail.com>
2023-10-29 10:54:45 +0530

    php programs

---
+++ function_(W_argu_WO_rv).php
@@ -0,0 +1,11 @@
+<?php 
+      
+      //In this php program we define a function with arugument without return value
+      function add($a,$b) //function define
+      {
+        $c = $a + $b;
+        echo"Addition : $c";
+      }
+      
+      add(19,23); //function call
+?>
---
+++ function_(W_argu_W_rv).php
@@ -0,0 +1,11 @@
+<?php 
+      
+      //In this function we define a function with argument with return value
+      function add($a, $b)
+      {
+        $z = $a + $b;
+        return $z;
+      }
+      $p = add(19,23);
+      echo"Addition : $p";
+?>
